19468d8f7aadea85a7316c93ce85204759d4c9ca
LongGuo Jin <may-dragon2000@outlook.com>
2022-10-13 23:13:33 +0800

    fix

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // mongoURI: "mongodb+srv://maydragon2000:maydragon2000@maydragon.0athwjd.mongodb.net/?retryWrites=true&w=majority",
-    mongoURI:"mongodb://127.0.0.1:27017",
-    secretOrKey: "secret",
-    SENDGRID_API:"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"
-};
